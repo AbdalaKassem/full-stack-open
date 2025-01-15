0ccbbe73d0c0bdbe688caaca808c7f6bbbf7520b
abdala kassem <abdalakassem@abdalas-MacBook-Air.local>
2025-01-15 23:19:47 +0200

    commit step 4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -12,44 +12,56 @@ const Part = (props) => {
   )
 }
 const Content = (props) => {
+  // props.parts.map(element => {
+  //   console.log(element)
+  // })
   // console.log(props.exercises)
   return (
     <>
-      <Part name={props.part1.name} exercises={props.part1.exercises}/>
-      <Part name={props.part2.name} exercises={props.part2.exercises}/>
-      <Part name={props.part3.name} exercises={props.part3.exercises}/>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises}/>
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises}/>
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises}/>
+      {/* {props.parts.map(element => (
+        <Part name={element.name} exercises={element.exercises}/>
+      ))} */}
     </>    
   )
 }
 
 const Total = (props) => {
+  let sum = 0
+  props.parts.forEach(e => {
+    sum += e.exercises
+  })
   return (
     <div>
-      Number of exercises {props.sum}
+      Number of exercises {sum}
     </div>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
     
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3}/>
-      <Total sum={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts}/>
+      <Total parts={parts} />
     </div>
   )
 }
